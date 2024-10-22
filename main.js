517d425d45640d49e660902bb96eae1a935ed05d
Nabi <smohamed.diop@gmail.com>
2024-10-22 14:07:24 +0100

    exo finish

---
+++ main.js
@@ -0,0 +1,19 @@
+// Créez un objet "personne" avec les propriétés suivantes : nom, prénom, âge, profession. Affichez cet objet dans la console.
+let personne = {
+  Prenom: "John",
+  nom: "Doe",
+  age: 24,
+  profession: "full-stack DEV",
+};
+console.log(personne);
+// Modifiez une propriété de l'objet personne (par exemple, l'âge) et affichez à nouveau l'objet modifié dans la console.
+personne.age = 25;
+console.log(personne);
+// Créez un deuxième objet "animal" avec les propriétés suivantes : espèce, couleur, nombre de pattes. Affichez cet objet dans la console.
+let animal = {
+  nom: "Lion",
+  espece: "Mammifères ",
+  couleur: "fauve",
+  Nombre_de_pattes: 4,
+};
+console.log(animal);
